747e024abb81091bbb239f552a8ac84a2b82ce75
lupeng@macbook <pengloo53@gmail.com>
2020-02-18 12:40:44 +0800

    update

---
+++ components/list-2p/list-2p.js
@@ -0,0 +1,34 @@
+// components/list-2p/list-2p.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    lists:{
+      type: Array,
+      value: []
+    },
+    tap: {
+      type: String,
+      value: ''
+    },
+    press: {
+      type: String,
+      value: ''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
